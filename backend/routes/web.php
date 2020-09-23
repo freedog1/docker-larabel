a78865b95dfcafe8bd64a2b39ceda46ec0c7dba7
freedog1 <koki375656@gmail.com>
2020-09-24 00:00:17 +0900

    feat larabel install

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/a/{str}',function ($str) {
+    return view('a',compact('str'));
 });
+
+//Route::get('/','PageController@index');
+Route::get('/',[PageController::class,'index']);
